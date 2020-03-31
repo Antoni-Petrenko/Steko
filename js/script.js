f37f6d0c6f7f5df0b4e82681debc70bbd6969f10
Петренко Антон Володимирович <petrenko_av@steko.net>
2020-03-31 14:44:22 +0300

    add header and preload animation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const page = {
   preloader: document.querySelector(".preloader"),
+  headerBackground: document.querySelector(".header__background"),
   firstSsectionImg: document.querySelector(".first-section__image-box--image"),
+  header: document.querySelector(".header"),
   footerSection: document.querySelector(".footer__content-box"),
   sections: [
     document.querySelector(".second-section"),
@@ -10,12 +12,56 @@ const page = {
     document.querySelector(".sixth-section")
   ],
   svgRects: document.querySelectorAll("[class*='__clone__SVG']"),
-  preloaderAnimation:anime.timeline({
+  preloaderAnimation: anime.timeline({
     easing: 'easeOutExpo',
     duration: 1000
   })
 
 };
+
+
+window.onbeforeunload=()=>window.scrollTo(0,0);
+let isScrollAnimationON=true;
+const scroll = e => {
+  e.preventDefault();
+  if(isScrollAnimationON){
+    console.log("ok")
+    isScrollAnimationON=false
+  anime.timeline({
+    targets:page.headerBackground,
+    delay:500,
+    duration: 500,
+    endDelay: 400,
+    easing: 'easeInOutSine'
+  }).add({
+    opacity:[0,1],
+  }).add({
+    targets:page.header,
+    opacity:0
+  }).add({
+    delay: 100,
+    opacity:[1,0],
+    complete:()=>{
+      page.headerBackground.style.pointerEvent="none";
+      page.header.remove();
+      window.removeEventListener("wheel",scroll);
+      window.removeEventListener("touchmove",scroll);
+      window.removeEventListener("scroll",scroll);
+    }
+  })
+  }else return
+}
+window.addEventListener("wheel", scroll, {
+  passive: false
+});
+window.addEventListener("touchmove", scroll, {
+  passive: false
+});
+window.addEventListener("scroll", scroll, {
+  passive: false
+});
+
+
 
 
 const setCloneSectionSize = section => {
@@ -27,7 +73,6 @@ const setCloneSectionSize = section => {
     selectClone.style.top = `${positionOnThePage}px`;
   }
 };
-
 
 const addObserver = (targetElement, callback, isDisconect) => {
   const observerOptions = {
@@ -50,19 +95,19 @@ const addObserver = (targetElement, callback, isDisconect) => {
 
 document.addEventListener('DOMContentLoaded', () => {
   page.preloaderAnimation.add({
-    targets:page.preloader.children[0],
+    targets: page.preloader.children[0],
     translateX: "-50%",
-    translateY:"-50%"
+    translateY: "-50%"
   }).add({
-    targets:page.preloader.children[0],
+    targets: page.preloader.children[0],
     height: "100%",
   }).add({
     targets: page.preloader.children[1],
     easing: 'easeOutExpo',
     duration: 1000,
-    opacity:1,
-    translateX:["-50%", "-50%"],
-    translateY:["-42%","-50%"]
+    opacity: 1,
+    translateX: ["-50%", "-50%"],
+    translateY: ["-42%", "-50%"]
   });
   window.addEventListener("scroll", () => (page.firstSsectionImg.style.transform = `scale(${1.2 - window.pageYOffset / 2000})`));
   window.addEventListener("resize", e => (e.target.requestIdleCallback(() => page.sections.forEach(setCloneSectionSize))));
@@ -96,13 +141,13 @@ document.addEventListener('DOMContentLoaded', () => {
 })
 
 window.onload = () => {
- page.preloaderAnimation.add({
-    targets:page.preloader,
+  page.preloaderAnimation.add({
+    targets: page.preloader,
     easing: 'easeOutExpo',
     duration: 2000,
-    opacity:0
+    opacity: 0
   })
-  page.preloaderAnimation.finished.then((el)=>{
+  page.preloaderAnimation.finished.then((el) => {
     page.preloader.remove()
   });
   page.sections.forEach(setCloneSectionSize);
